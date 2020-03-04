d23df9f4c523efad7e5cfe60dcc1d1091830011a
Jussi Vatjus-Anttila <jussiva@gmail.com>
2020-03-04 18:08:59 +0200

    fix circlefile

---
+++ test/__init__.py
@@ -0,0 +1,10 @@
+import unittest
+from .test_api import TestClient
+from .test_api_build import TestBuild
+from .test_api_result import TestResult
+from .test_tools import TestTools
+from .test_base import TestBase
+from .test_logger import TestLogger
+from .test_query import TestRequest
+from .test_transport import TestRequest
+from .test_cli import TestCli
---
+++ test/test_cli.py
@@ -0,0 +1,10 @@
+# pylint: disable=missing-docstring
+
+import unittest
+from mock import patch, Mock
+
+
+class TestCli(unittest.TestCase):
+
+    def test_dummy(self):
+        pass
